bfe0c6faf97f00b46ca907ca848b4b370c18ae0d
Eric Liu <eliu1@andrew.cmu.edu>
2020-04-24 22:46:22 -0400

    manager abilities tests pass

---
+++ app/models/ability.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    user ||= Employee.new # guest user (not logged in)
+
+    if user.role? :admin
+      can :manage, :all
+    elsif user.role? :manager
+      can :manage, Shift
+      can :manage, ShiftJob
+      can :index, Store
+      can :show, Store do |store|
+        store.id == user.current_assignment.store_id
+      end
+      can :index, Assignment
+      can :show, Assignment do |assignment|
+        assignment.store_id == user.current_assignment.store_id
+      end
+      can :index, Employee
+      can :show, Employee do |employee|
+        employee.current_assignment.store_id == user.current_assignment.store_id
+      end
+      can :edit, Employee do |employee|
+        employee.current_assignment.store_id == user.current_assignment.store_id
+      end
+      can :update, Employee do |employee|
+        employee.current_assignment.store_id == user.current_assignment.store_id
+      end
+    else
+      can :read, :all
+    end
+
+  end
+end
